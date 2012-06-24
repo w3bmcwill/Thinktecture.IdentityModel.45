ae0a201761679729e4bdcc8978795b4898361a14
Dominick Baier <dbaier@leastprivilege.com>
2012-06-24 08:10:23 +0200

    Added conversion from HttpRequest to HttpRequestMessage

---
+++ IdentityModel/Thinktecture.IdentityModel/Extensions/HttpRequestMessageExtensions.cs
@@ -0,0 +1,79 @@
+﻿/*
+ * Copyright (c) Dominick Baier.  All rights reserved.
+ * see license.txt
+ */
+
+using System;
+using System.Net.Http;
+using System.Web;
+
+namespace Thinktecture.IdentityModel.Extensions
+{
+    public static class HttpRequestMessageExtensions
+    {
+        public static HttpRequestMessage ToHttpRequestMessage(this HttpRequestBase httpRequest)
+        {
+            HttpMethod httpMethod = GetHttpMethod(httpRequest.HttpMethod);
+            Uri url = httpRequest.Url;
+            
+            HttpRequestMessage httpRequestMessage = new HttpRequestMessage(httpMethod, url)
+            {
+                Content = new StreamContent(httpRequest.InputStream)
+            };
+
+            foreach (string str in httpRequest.Headers)
+            {
+                string[] values = httpRequest.Headers.GetValues(str);
+                AddHeaderToHttpRequestMessage(httpRequestMessage, str, values);
+            }
+            
+            return httpRequestMessage;
+        }
+
+        internal static HttpMethod GetHttpMethod(string method)
+        {
+            if (string.IsNullOrEmpty(method))
+            {
+                return null;
+            }
+            if (string.Equals("GET", method, StringComparison.OrdinalIgnoreCase))
+            {
+                return HttpMethod.Get;
+            }
+            if (string.Equals("POST", method, StringComparison.OrdinalIgnoreCase))
+            {
+                return HttpMethod.Post;
+            }
+            if (string.Equals("PUT", method, StringComparison.OrdinalIgnoreCase))
+            {
+                return HttpMethod.Put;
+            }
+            if (string.Equals("DELETE", method, StringComparison.OrdinalIgnoreCase))
+            {
+                return HttpMethod.Delete;
+            }
+            if (string.Equals("HEAD", method, StringComparison.OrdinalIgnoreCase))
+            {
+                return HttpMethod.Head;
+            }
+            if (string.Equals("OPTIONS", method, StringComparison.OrdinalIgnoreCase))
+            {
+                return HttpMethod.Options;
+            }
+            if (string.Equals("TRACE", method, StringComparison.OrdinalIgnoreCase))
+            {
+                return HttpMethod.Trace;
+            }
+
+            return new HttpMethod(method);
+        }
+
+        private static void AddHeaderToHttpRequestMessage(HttpRequestMessage httpRequestMessage, string headerName, string[] headerValues)
+        {
+            if (!httpRequestMessage.Headers.TryAddWithoutValidation(headerName, headerValues))
+            {
+                httpRequestMessage.Content.Headers.TryAddWithoutValidation(headerName, headerValues);
+            }
+        }
+    }
+}
