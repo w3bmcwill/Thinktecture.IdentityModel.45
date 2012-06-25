601382cdd2fa751fbcb22a4c83ff6b4852faf9c8
Dominick Baier <dbaier@leastprivilege.com>
2012-06-25 22:12:54 +0200

    Updated casing

---
+++ IdentityModel/Thinktecture.IdentityModel/Http/Cors/WebApi/WebApiCorsConfiguration.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Copyright (c) Dominick Baier & Brock Allen.  All rights reserved.
+ * see license.txt
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Thinktecture.IdentityModel.Http.Cors.WebApi
+{
+    public class WebApiCorsConfiguration : CorsConfiguration
+    {
+        public void RegisterGlobal(HttpConfiguration httpConfig)
+        {
+            httpConfig.MessageHandlers.Add(new CorsMessageHandler(this, httpConfig));
+        }
+
+        public DelegatingHandler CreateHandler(HttpConfiguration httpConfig, DelegatingHandler innerHandler = null)
+        {
+            return new CorsMessageHandler(this, httpConfig)
+            {
+                InnerHandler = innerHandler
+            };
+        }
+    }
+}
