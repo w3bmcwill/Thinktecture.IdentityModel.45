98c6d4a2adc291ea758849873844da33cabd4a77
Dominick Baier <dbaier@leastprivilege.com>
2012-06-28 10:02:57 +0200

    Added JS session token sample

---
+++ Samples/Web API/Clients/JavaScriptSessionTokenClient/scripts/sessionTokenAuthentication.js
@@ -0,0 +1,57 @@
+﻿thinktectureIdentityModel.SessionTokenAuthentication = function (endpoint) {
+    that = this;
+    that.endpointAddress = endpoint;
+    that.store = new Lawnchair({ name: 'tt.idm.sessionToken', adapter: 'dom' }, function () {
+    });
+};
+
+thinktectureIdentityModel.SessionTokenAuthentication.prototype = function () {
+    requestSessionToken = function (un, pw, callback) {
+        $.ajax({
+            type: 'GET',
+            cache: true,
+            url: that.endpointAddress,
+            beforeSend: function (xhr) { 
+                xhr.setRequestHeader('Authorization', createBasicAuthenticationHeader(un, pw)); 
+            },
+            success: function (result) {                
+                that.store.save({ key: that.endpointAddress, sessionToken: result.access_token }, function (obj) {
+                });
+
+                callback(result.access_token);
+            },
+            error: function (error) {
+                if (error.status == 401) {
+                    alert('Unauthorized');
+                }
+                else {
+                    alert('Error calling service: ' + error.responseText);
+                }
+            }
+        });
+    };
+
+    createBasicAuthenticationHeader = function (un, pw) {
+        var header = 'Basic ' + $.base64.encode(un + ":" + pw);
+
+        return header;
+    };
+
+    getSessionTokenAuthenticationHeader = function (token) {
+        var header = 'Session ' + token;
+
+        return header;
+    };
+
+    getLocalSessionToken = function (key, callback) {
+        that.store.get(key, function (data) {
+            callback(data.sessionToken);
+        });        
+    };
+
+    return {
+        requestSessionToken: requestSessionToken,
+        getSessionTokenAuthenticationHeader: getSessionTokenAuthenticationHeader,
+        getLocalSessionToken: getLocalSessionToken
+    };
+}();
---
+++ Samples/Web API/Clients/JavaScriptSessionTokenClient/scripts/sessionTokenClient.js
@@ -0,0 +1,47 @@
+﻿var tokenEndpoint = 'https://adfs.leastprivilege.vm/webapisecurity/api/token',
+    serviceEndpoint = 'https://adfs.leastprivilege.vm/webapisecurity/api/identity',
+    authN;
+
+$(function () {
+    authN = new thinktectureIdentityModel.SessionTokenAuthentication(tokenEndpoint);
+});
+
+function authenticate() {
+    $('#sessionToken').val('');
+    $('#data').empty();
+
+    var un = $('#username').val(),
+        pw = $('#password').val();
+
+    authN.requestSessionToken(un, pw, tokenAvailable);
+};
+
+function tokenAvailable(sessionToken) {
+    $('#sessionToken').val(sessionToken);
+};
+
+function callService() {
+    authN.getLocalSessionToken(tokenEndpoint, function (token) {
+        $.ajax({
+            type: 'GET',
+            cache: true,
+            url: this.serviceEndpoint,
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('Authorization', authN.getSessionTokenAuthenticationHeader(token));
+            },
+            success: function (result) {
+                $.each(result.Claims, function () {
+                    $('#claims').append($('<li>').text(this.ClaimType + ':  ' + this.Value));
+                });
+            },
+            error: function (error) {
+                if (error.status == 401) {
+                    alert('Unauthorized');
+                }
+                else {
+                    alert('Error calling service: ' + error.responseText);
+                }
+            }
+        });
+    });
+}
