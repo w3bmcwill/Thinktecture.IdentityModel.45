fd2d67eeee2b9a67f9e3ddb533e13c1f756a8c50
Dominick Baier <dbaier@leastprivilege.com>
2012-07-12 18:25:37 +0200

    MVC Sample: Added bootstrap token viewer

--- Samples/MVC and WCF/Web/Controllers/UserController.cs
+++ Samples/MVC and WCF/Web/Controllers/UserController.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IdentityModel.Services;
+using System.IdentityModel.Tokens;
+using System.IO;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml;
 
 namespace Web.Controllers
 {
@@ -13,6 +18,51 @@ namespace Web.Controllers
         public ActionResult Index()
         {
             return View("Identity", HttpContext.User);
+        }
+
+        public ActionResult Token()
+        {
+            string tokenString;
+            
+            try
+            {
+                tokenString = GetBootstrapTokenAsString(User.Identity as ClaimsIdentity);
+            }
+            catch (Exception ex)
+            {
+                return new ContentResult
+                {
+                    Content = ex.Message
+                };
+            }
+
+            return new ContentResult
+            {
+                ContentType = "text/xml",
+                Content = tokenString
+            };
+        }
+
+        private string GetBootstrapTokenAsString(ClaimsIdentity identity)
+        {
+            var context = identity.BootstrapContext as BootstrapContext;
+
+            if (context == null)
+            {
+                throw new InvalidOperationException("Bootstrap context is null");
+            }
+
+            if (!string.IsNullOrWhiteSpace(context.Token))
+            {
+                return context.Token;
+            }
+
+            var sb = new StringBuilder(128);
+            context.SecurityTokenHandler.WriteToken(
+                new XmlTextWriter(new StringWriter(sb)),
+                context.SecurityToken);
+
+            return sb.ToString();
         }
 
         public ActionResult Signout()
